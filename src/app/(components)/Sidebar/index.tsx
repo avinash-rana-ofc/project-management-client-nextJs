49d191eb2582846feef993368d814838f8740b9b
Avinash <avinashrana55@yahoo.com>
2025-04-21 23:11:25 +0530

    Updated Sidebar css for active menu

--- src/app/(components)/Sidebar/index.tsx
+++ src/app/(components)/Sidebar/index.tsx
@@ -144,13 +144,14 @@ const SidebarLink = ({ href, icon: Icon, label }: SidebarLinkProps) => {
   const isActive =
     pathname === href || (pathname === "/" && href === "/dashboard");
   console.log(isActive);
+  console.log(href);
   return (
     <Link href={href} className="w-full">
       <div
         className={`relative flex cursor-pointer items-center gap-3 transition-colors hover:bg-gray-100 dark:bg-black dark:hover:bg-gray-700 ${isActive ? "bg-gray-100 text-white dark:bg-gray-600" : ""} justify-start px-8 py-3`}
       >
         {isActive && (
-          <div className="w-5[px] absolute left-0 top-0 h-[100%] bg-blue-200" />
+          <div className="w-[5px] absolute left-0 top-0 h-[100%] bg-blue-200" />
         )}
         <Icon className="h-6 w-6 text-gray-800 dark:text-gray-100" />
         <span className="font-medium text-gray-800 dark:text-gray-100">
